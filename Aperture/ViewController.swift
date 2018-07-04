284e808abc207ee38514a96b9088ad3133ccde5f
Roy Li <roy.li.zixuan@gmail.com>
2018-07-04 15:04:28 -0700

    Initial Commit

---
+++ Aperture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aperture
+//
+//  Created by Roy Li on 7/4/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
